a2e4c9c7909d3dedb190d35db6c7bde2d474e37f
Kynan Rilee <kynan.rilee@gmail.com>
2018-02-14 16:07:29 -0800

    turn off tls everywhere

--- config/clientscript.sh
+++ config/clientscript.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
 set -e
+set -x
 
 source ./clientenv
 
-peer channel create -o orderer:7050 -c blubc -f ./channel-artifacts/channel.tx --tls --cafile /workspace/crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+peer channel create -o orderer:7050 -c blubc -f ./channel-artifacts/channel.tx
 
 peer channel join -b blubc.block
 
 peer chaincode install -n mycc -v 1.0 -p chaincode
 
-peer chaincode instantiate -o orderer:7050 --tls --cafile /workspace/crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -C blubc -n mycc -v 1.0 -c '{"Args":["init","a", "100", "b","200"]}' -P "OR ('Org1MSP.member','Org2MSP.member')"
+peer chaincode instantiate -o orderer:7050 -C blubc -n mycc -v 1.0 -c '{"Args":["init","a", "100", "b","200"]}' -P "OR ('Org1MSP.member','Org2MSP.member')"
+
+#peer chaincode query -C blubc -n mycc -c '{"Args":["query","a"]}'
