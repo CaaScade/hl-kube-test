3097ad51fb18372c582d231d64e000384e73cb68
Kynan Rilee <kynan.rilee@gmail.com>
2018-02-18 15:05:01 -0800

    build and deploy config from cli tool

---
+++ fabric-dev/ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for fabric-dev
+
+## Unreleased changes
---
+++ fabric-dev/app/Main.hs
@@ -0,0 +1,119 @@
+{-# LANGUAGE ExtendedDefaultRules #-}
+{-# LANGUAGE NoImplicitPrelude    #-}
+{-# LANGUAGE OverloadedStrings    #-}
+{-# OPTIONS_GHC -fno-warn-type-defaults #-}
+
+module Main where
+
+import           Import
+
+import qualified Data.Text as T
+import           Shelly
+import qualified Shelly as SH
+
+{-
+This program expects the following directory structure and files:
+root:
+  bin:
+    cryptogen
+  config:
+    crypto-config.yaml
+-}
+
+main :: IO ()
+main = shelly . verbosely $ do
+  buildConfig root channelID
+  bundleConfig root
+  where root = "/home/kynan/workspace/go/src/github.com/koki/fabric-dev/fabric-dev/testroot"
+        channelID = "blubc"
+
+buildConfig :: SH.FilePath -> Text -> Sh ()
+buildConfig root channelID = do
+  prependToPath $ root <> "bin"
+  cd root
+  rm_rf "./crypto-config"
+  -- Write crypto-config into the config directory because configtxgen can only look there.
+  run_ "cryptogen" ["generate", "--config=./config/crypto-config.yaml", "--output=./config/crypto-config"]
+  mkdir_p "./channel-artifacts"
+  currentDir <- pwd
+  setenv "FABRIC_CFG_PATH" =<< toTextWarn (currentDir <> "config")
+  run_ "configtxgen" ["-profile", "TwoOrgsOrdererGenesis", "-outputBlock", "./channel-artifacts/genesis.block"]
+  run_ "configtxgen" ["-profile", "TwoOrgsChannel", "-outputCreateChannelTx", "./channel-artifacts/channel.tx", "-channelID", channelID]
+  run_ "configtxgen" ["-profile", "TwoOrgsChannel", "-outputAnchorPeersUpdate", "./channel-artifacts/Org1MSPanchors.tx", "-channelID", channelID, "-asOrg", "Org1MSP"]
+  run_ "configtxgen" ["-profile", "TwoOrgsChannel", "-outputAnchorPeersUpdate", "./channel-artifacts/Org2MSPanchors.tx", "-channelID", channelID, "-asOrg", "Org2MSP"]
+  -- Put the crypto-config where it belongs.
+  mv "config/crypto-config" root
+
+-- NOTE: *SecretName functions need to ensure lowercase & no periods
+-- TODO: k8s namespace
+bundleConfig :: SH.FilePath -> Sh ()
+bundleConfig root = do
+  let peerOrgs = ["org1", "org2"]
+      peerPeers = ["peer0", "peer1"]
+      peerUsers = ["Admin", "User1"]
+      folders = ["msp", "tls"]
+  cd root
+  mkdir_p "config-artifacts"
+  sequence_ $ bundlePeer <$> peerPeers <*> peerOrgs <*> folders
+  sequence_ $ bundleOrderer <$> folders
+  sequence_ $ bundleUser "peer" <$> peerUsers <*> peerOrgs <*> folders
+  sequence_ $ bundleUser "orderer" "Admin" "example.com" <$> folders
+  deploySecret "orderer-genesis-block" "channel-artifacts/genesis.block" "file"
+  deployConfigMap "core-yaml" "config/core.yaml" "file"
+  deployConfigMap "orderer-yaml" "config/orderer.yaml" "file"
+  where
+    bundlePeer peer org folder =
+      let input = peerFolderName peer org
+          output = inOutputFolder $ peerBundleName peer org folder
+          deploy = peerSecretName peer org folder
+      in do run_ "tar" ["-zcf", output, "-C", input, folder]
+            deploySecret deploy output "bundle"
+    peerBundleName peer org folder =
+      T.intercalate "." [peer, org, folder, "tar.gz"]
+    peerFolderName peer org =
+      T.intercalate
+        "/"
+        ["crypto-config/peerOrganizations", org, "peers", peer <> "." <> org]
+    peerSecretName peer org folder = T.intercalate "-" [peer, org, folder]
+    bundleOrderer folder =
+      let input = ordererFolderName
+          output = inOutputFolder $ ordererBundleName folder
+          deploy = ordererSecretName folder
+      in do run_ "tar" ["-zcf", output, "-C", input, folder]
+            deploySecret deploy output "bundle"
+    ordererBundleName folder =
+      T.intercalate "." ["orderer.example.com", folder, "tar.gz"]
+    ordererFolderName =
+      "crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com"
+    ordererSecretName folder = "orderer-example-com-" <> folder
+    bundleUser t user org folder =
+      let input = userFolderName t user org
+          output = inOutputFolder $ userBundleName user org folder
+          deploy = userSecretName user org folder
+      in do
+        run_ "tar" ["-zcf", output, "-C", input, folder]
+        deploySecret deploy output "bundle"
+    userBundleName user org folder =
+      T.intercalate "." [user <> "@" <> org, folder, "tar.gz"]
+    userFolderName t user org =
+      T.intercalate
+        "/"
+        [ "crypto-config"
+        , t <> "Organizations"
+        , org
+        , "users"
+        , user <> "@" <> org
+        ]
+    userSecretName user org folder =
+      T.intercalate "-" [T.toLower user, org, folder]
+    deploySecret name file key = do
+      let delete = run_ "kubectl" ["delete", "secret", name]
+          options = T.intercalate "=" ["--from-file", key, file]
+      catchany_sh delete (const $ return ())
+      run_ "kubectl" ["create", "secret", "generic", name, options]
+    deployConfigMap name file key = do
+      let delete = run_ "kubectl" ["delete", "configmap", name]
+          options = T.intercalate "=" ["--from-file", key, file]
+      catchany_sh delete (const $ return ())
+      run_ "kubectl" ["create", "configmap", name, options]
+    inOutputFolder file = "config-artifacts" <> "/" <> file
